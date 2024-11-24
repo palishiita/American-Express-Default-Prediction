e4b2ca641b8fa563d4b44ff7a9e5c62b75c512fe
Ishiita_Pal <ishiitapal@gmail.com>
2024-11-24 23:07:11 +0100

    converting to parquet

--- ADF/README.md
+++
@@ -1,2 +0,0 @@
-# Azure Data Factory
-Simple ADF pipeline to convert zip file containing .csv files into .parquet and uploading it to the datalake
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -118,31 +118,3 @@ resource "azurerm_machine_learning_compute_instance" "aml_compute_instance" {
     type = "SystemAssigned"  # Assign a system-assigned managed identity
   }
 }
-
-# Create Azure Data Factory without GitHub integration
-resource "azurerm_data_factory" "adf" {
-  name                = "americanexpress-adf"
-  location            = data.azurerm_resource_group.aml_rg.location
-  resource_group_name = data.azurerm_resource_group.aml_rg.name
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  public_network_enabled = true
-}
-
-# Linking Azure Data Lake Storage Gen2 Account as Linked Service in Data Factory
-resource "azurerm_data_factory_linked_service_azure_blob_storage" "adf_linked_adls" {
-  name            = "adls-gen2-link"  # Updated name for clarity
-  data_factory_id = azurerm_data_factory.adf.id
-
-  # Account key authentication (Replace with your storage account's actual name and key)
-  connection_string = data.azurerm_storage_account.aml_storage_account.primary_connection_string
-
-  # Alternatively, use Managed Identity authentication if desired:
-  use_managed_identity = true
-
-  # Optional description
-  description = "Linked Service to connect Azure Data Lake Gen2 to Azure Data Factory."
-}
